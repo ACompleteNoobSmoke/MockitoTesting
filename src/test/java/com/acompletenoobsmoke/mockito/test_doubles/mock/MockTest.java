418f8c635557aeab114c53b92d2913309b1042d3
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-06-14 03:08:13 -0500

    Did a couple verify testing

--- src/test/java/com/acompletenoobsmoke/mockito/test_doubles/mock/MockTest.java
+++ src/test/java/com/acompletenoobsmoke/mockito/test_doubles/mock/MockTest.java
@@ -1,12 +1,15 @@
 package com.acompletenoobsmoke.mockito.test_doubles.mock;
 
 import com.acompletenoobsmoke.mockito.test_doubles.spy.BookService;
+import com.acompletenoobsmoke.mockito.test_doubles.stub.Book;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class MockTest {
 
@@ -26,5 +29,23 @@ public class MockTest {
         bookService.addBook(book2);
 
         bookRepositorySpy.verify(book2, 2);
+    }
+
+    @Test
+    public void demoMockWithMockito(){
+        BookRepository bookRepository = mock(BookRepository.class);
+        com.acompletenoobsmoke.mockito.test_doubles.mock.BookService bookService
+                = new com.acompletenoobsmoke.mockito.test_doubles.mock.BookService(bookRepository);
+
+        com.acompletenoobsmoke.mockito.test_doubles.mock.Book newBook1 =
+                new com.acompletenoobsmoke.mockito.test_doubles.mock.Book("1122", "The Bible", 500, LocalDate.now());
+        com.acompletenoobsmoke.mockito.test_doubles.mock.Book newBook2 =
+                new com.acompletenoobsmoke.mockito.test_doubles.mock.Book("1234", "Preacher", 700, LocalDate.now());
+
+        bookService.addBook(newBook1);
+        bookService.addBook(newBook2);
+
+        verify(bookRepository).save(newBook1);
+        verify(bookRepository).save(newBook2);
     }
 }
